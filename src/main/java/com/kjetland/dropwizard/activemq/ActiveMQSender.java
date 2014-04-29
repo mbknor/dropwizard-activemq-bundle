1d1aad7523ab4c4304e5766700e0038f75ac7dba
Oddmar Sandvik <oddmar.sandvik@gmail.com>
2014-04-29 17:00:43 +0200

    Extend ActiveMQSender to accept a function for creating a message.

--- src/main/java/com/kjetland/dropwizard/activemq/ActiveMQSender.java
+++ src/main/java/com/kjetland/dropwizard/activemq/ActiveMQSender.java
@@ -1,7 +1,11 @@
 package com.kjetland.dropwizard.activemq;
 
+import javax.jms.Message;
+import javax.jms.Session;
+
 public interface ActiveMQSender {
 
     void sendJson(String json);
     void send(Object object);
+    void send(JMSFunction<Session, Message> messageCreator);
 }
--- src/main/java/com/kjetland/dropwizard/activemq/ActiveMQSenderImpl.java
+++ src/main/java/com/kjetland/dropwizard/activemq/ActiveMQSenderImpl.java
@@ -4,7 +4,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.jms.*;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -70,7 +78,7 @@ public class ActiveMQSenderImpl implements ActiveMQSender {
                 final MessageProducer messageProducer = session.createProducer(d);
                 try {
                     messageProducer.setDeliveryMode(persistent ? DeliveryMode.PERSISTENT : DeliveryMode.NON_PERSISTENT);
-                    if ( timeToLiveInSeconds.isPresent() ) {
+                    if (timeToLiveInSeconds.isPresent()) {
                         messageProducer.setTimeToLive(TimeUnit.SECONDS.toMillis(timeToLiveInSeconds.get()));
                     }
 
@@ -79,17 +87,55 @@ public class ActiveMQSenderImpl implements ActiveMQSender {
                     messageProducer.send(textMessage);
 
                 } finally {
-                    ActiveMQUtils.silent( () -> messageProducer.close() );
+                    ActiveMQUtils.silent(() -> messageProducer.close());
                 }
             } finally {
-                ActiveMQUtils.silent( () -> session.close() );
+                ActiveMQUtils.silent(() -> session.close());
             }
 
         } finally {
-            ActiveMQUtils.silent( () -> connection.close() );
+            ActiveMQUtils.silent(() -> connection.close());
         }
         final Session session;
 
 
+    }
+
+    @Override
+    public void send(JMSFunction<Session, Message> messageCreator) {
+        // Since we're using the pooled connectionFactory,
+        // we can create connection, session and producer on the fly here.
+        // as long as we do the cleanup / return to pool
+
+        try {
+            final Connection connection = connectionFactory.createConnection();
+            try {
+
+                final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+                try {
+
+                    final Destination d = destinationCreator.create(session, destination);
+                    final MessageProducer messageProducer = session.createProducer(d);
+                    try {
+                        messageProducer.setDeliveryMode(persistent ? DeliveryMode.PERSISTENT : DeliveryMode.NON_PERSISTENT);
+                        if (timeToLiveInSeconds.isPresent()) {
+                            messageProducer.setTimeToLive(TimeUnit.SECONDS.toMillis(timeToLiveInSeconds.get()));
+                        }
+                        final Message message = messageCreator.apply(session);
+                        messageProducer.send(message);
+
+                    } finally {
+                        ActiveMQUtils.silent(() -> messageProducer.close());
+                    }
+                } finally {
+                    ActiveMQUtils.silent(() -> session.close());
+                }
+
+            } finally {
+                ActiveMQUtils.silent(() -> connection.close());
+            }
+        } catch ( JMSException jmsException) {
+            throw new RuntimeException("Error sending to jms", jmsException);
+        }
     }
 }
